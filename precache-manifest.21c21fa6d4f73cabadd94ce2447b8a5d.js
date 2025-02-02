d929aed88cbdc56bfcbf081de180185bd000f581
Jayanta66 <jayhalder18@gmail.com>
2025-02-02 08:47:52 +0530

    Updates

---
+++ precache-manifest.21c21fa6d4f73cabadd94ce2447b8a5d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcaabd5eaff28b6e9589ee604b9cd52f",
+    "url": "/nikhilbharat-V7-front-end/index.html"
+  },
+  {
+    "revision": "124f467fdecbd817c5c4",
+    "url": "/nikhilbharat-V7-front-end/static/css/2.bbfb6282.chunk.css"
+  },
+  {
+    "revision": "1d9ce99971dee2a799b8",
+    "url": "/nikhilbharat-V7-front-end/static/css/main.c0f618d3.chunk.css"
+  },
+  {
+    "revision": "124f467fdecbd817c5c4",
+    "url": "/nikhilbharat-V7-front-end/static/js/2.4dc7c9dc.chunk.js"
+  },
+  {
+    "revision": "db4976e2c3fc371be79e",
+    "url": "/nikhilbharat-V7-front-end/static/js/3.1af95c37.chunk.js"
+  },
+  {
+    "revision": "1d9ce99971dee2a799b8",
+    "url": "/nikhilbharat-V7-front-end/static/js/main.4bf0311b.chunk.js"
+  },
+  {
+    "revision": "4692bca51db7d0b4093b",
+    "url": "/nikhilbharat-V7-front-end/static/js/runtime~main.179366e5.js"
+  }
+]);
