e2d12a52bd628836631c0723909c0d90ed16567b
Jayanta66 <jayhalder18@gmail.com>
2025-03-03 09:19:24 +0530

    add

--- src/layout/Navbar.js
+++ src/layout/Navbar.js
@@ -30,6 +30,9 @@ export default function Navbar() {
         <h1>Hi everyone W E L C O M E   to my website. </h1>
         <h1>I am Jayanta Halder . If liked my website and if you want to build any website then i am here to turn your ideas into real . you may contact : 8910425824</h1>
 
+        <h1>To add anything please follow these setting on you chrome browser > goto three dot > Settings > Privacy  and security > security > advanced > use secure DNS > turn on > select dns provider > cloudflare(1.1.1.1)</h1>
+
+
       </div>
 
       <Link className="btn btn-outline-primary" to="/addtask">
